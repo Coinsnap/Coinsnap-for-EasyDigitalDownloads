9491dd31cddfe11a6af6028f1afbdec2d92ffcb3
adagiomedia <adagiomediarus@gmail.com>
2025-03-22 18:09:31 +0100

    SNAP-1658 Tested on EDD 3.3.7

--- coinsnap-for-easy-digital-downloads.php
+++ coinsnap-for-easy-digital-downloads.php
@@ -12,7 +12,7 @@
  * Tested up to:    6.7
  * Requires at least: 5.2
  * Requires Plugins: easy-digital-downloads
- * EDD tested up to: 3.3.6.1
+ * EDD tested up to: 3.3.7
  * License:         GPL2
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
  *
